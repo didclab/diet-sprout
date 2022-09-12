4bc335c605970dab97fd67d1d7583be2f7617610
Elvis Rodrigues <elvis@elrodrigues.com>
2022-09-12 19:41:35 -0400

    minor changes

--- constants.py
+++ constants.py
@@ -5,3 +5,8 @@ Constants and Arguments for influx_data and poisson
 APP_NAME = "elvisdav@buffalo.edu-didclab-elvis-uc"
 USER_NAME = "elvisdav@buffalo.edu"
 QUERY_RANGE = "-1h"
+
+NUM_DISCRETE = 256
+# Should be independent of units
+MIN_THROUGHPUT = 0
+MAX_THROUGHPUT = 10
---
+++ poisson.py
@@ -0,0 +1,3 @@
+import constants as cons
+import pandas as pd
+import numpy as np
