18d4de7301f38126b1885730a1119e8f67021ae0
Elvis Rodrigues <elvis@elrodrigues.com>
2022-09-13 18:37:07 -0400

    added to poisson.py

--- constants.py
+++ constants.py
@@ -4,9 +4,9 @@ Constants and Arguments for influx_data and poisson
 
 APP_NAME = "elvisdav@buffalo.edu-didclab-elvis-uc"
 USER_NAME = "elvisdav@buffalo.edu"
-QUERY_RANGE = "-1h"
+QUERY_RANGE = "-1m"
 
-NUM_DISCRETE = 256
+NUM_DISCRETE = 255
 # Should be independent of units
 MIN_THROUGHPUT = 0
-MAX_THROUGHPUT = 10
+MAX_THROUGHPUT = 10  # Gigabits
--- poisson.py
+++ poisson.py
@@ -1,3 +1,18 @@
 import constants as cons
 import pandas as pd
 import numpy as np
+from math import sqrt
+from scipy.stats import norm
+
+# https://scipy-cookbook.readthedocs.io/items/BrownianMotion.html
+
+class PoissonDistribution:
+    def __init__(self, high, num_discrete, low=0.):
+        self.inputs = np.linspace(low, high, num=num_discrete)
+        self.distribution = np.array([1 / num_discrete for _ in range(num_discrete)])
+
+    def mean(self):
+        pass
+
+    def update_distribution(self, time, n_bytes):
+        pass
